b1da96f0b9b0f02e291ef8659fb9402e91f9aa9a
Armaan Hirani <ahirani3@student.gsu.edu>
2024-03-11 20:18:35 -0400

    This is the Dashboard UI

--- lib/screens/dashboard.dart
+++ lib/screens/dashboard.dart
@@ -1,36 +1,109 @@
 import 'package:flutter/material.dart';
 import '../controllers/dashboard_controller.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class Dashboard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final double buttonHeight = 60;
+    final double buttonWidth = 115;
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Dashboard'),
-      ),
+      backgroundColor: Colors.teal[300],
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            ElevatedButton(
-              child: Text('Log Expense'),
-              onPressed: () {
-                DashboardController.navigateToExpenseEntry(context);
-              },
-            ),
-            ElevatedButton(
-              child: Text('Budget Setup'),
-              onPressed: () {
-                DashboardController.navigateToBudgetSetup(context);
-              },
-            ),
-            ElevatedButton(
-              child: Text('Reports and Charts'),
-              onPressed: () {
-                DashboardController.navigateToReportsAndCharts(context);
-              },
+            Text(
+              "Welcome",
+              style: GoogleFonts.caveat(
+                color: Colors.white,
+                fontSize: 90,
+                fontWeight: FontWeight.bold,
+              ),
             ),
           ],
+        ),
+      ),
+      
+      bottomNavigationBar: Container(
+        decoration: BoxDecoration(
+          border: Border(
+            top: BorderSide(width: 5.0, color: Colors.white),
+          ),
+        ),
+        child: BottomAppBar(
+          color: Colors.teal[800],
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  minimumSize: Size(buttonWidth, buttonHeight),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                  side: const BorderSide(
+                    width: 2.0,
+                    color: Colors.black, // Adjust the color as needed
+                  ),
+                ),
+                child: Text(
+                  'Log\nExpense',
+                  textAlign: TextAlign.center,
+                  style: GoogleFonts.asapCondensed(
+                    color: Colors.black,
+                  ),
+                ),
+                onPressed: () {
+                  DashboardController.navigateToExpenseEntry(context);
+                },
+              ),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  minimumSize: Size(buttonWidth, buttonHeight),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                  side: const BorderSide(
+                    width: 2.0,
+                    color: Colors.black, // Adjust the color as needed
+                  ),
+                ),
+                child: Text(
+                  'Budget\nSetup',
+                  textAlign: TextAlign.center,
+                  style: GoogleFonts.asapCondensed(
+                    color: Colors.black,
+                  ),
+                ),
+                onPressed: () {
+                  DashboardController.navigateToBudgetSetup(context);
+                },
+              ),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  minimumSize: Size(buttonWidth, buttonHeight),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                  side: const BorderSide(
+                    width: 2.0,
+                    color: Colors.black, // Adjust the color as needed
+                  ),
+                ),
+                child: Text(
+                  'Reports\nand Charts',
+                  textAlign: TextAlign.center,
+                  style: GoogleFonts.asapCondensed(
+                    color: Colors.black,
+                  ),
+                ),
+                onPressed: () {
+                  DashboardController.navigateToReportsAndCharts(context);
+                },
+              ),
+            ],
+          ),
         ),
       ),
     );
