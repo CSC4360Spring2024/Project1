84d0f8808f1cf20196ddd9b27a7805bd4ff39a7a
Armaan Hirani <ahirani3@student.gsu.edu>
2024-03-15 02:31:40 -0400

    Budget Setup Dialogue Box

--- lib/utils/dialog_widgets.dart
+++ lib/utils/dialog_widgets.dart
@@ -1,20 +1,51 @@
 import 'package:flutter/material.dart';
 
 class DialogWidgets {
+  
   static void showExceedDialog(BuildContext context, String selectedCategory) {
     try {
       showDialog(
         context: context,
         builder: (BuildContext context) {
           return AlertDialog(
-            title: Text('Budget Exceeded'),
-            content: Text('The budget for $selectedCategory has been exceeded.'),
+            backgroundColor: Colors.tealAccent,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(30.0), // Add border radius
+              side: BorderSide(
+                color: Colors.black,
+                width: 3.0,
+              ),
+            ),
+            title: const Center(
+              child: Text(
+                'Budget Exceeded',
+                style: TextStyle(
+                  color: Colors.black, // Change text color
+                  fontWeight: FontWeight.bold, // Change font weight
+                  fontSize: 20, // Change font size
+                ),
+              ),
+            ),
+            content: Text(
+              'The budget for $selectedCategory has been exceeded.',
+              style: TextStyle(
+                fontSize: 17,
+              ),
+            ),
             actions: <Widget>[
-              TextButton(
-                onPressed: () {
-                  Navigator.of(context).pop();
-                },
-                child: Text('OK'),
+              Center(
+                child: TextButton(
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                  child: Text(
+                    'OK',
+                    style: TextStyle(
+                      color: Colors.black, // Change text color
+                      fontWeight: FontWeight.bold, // Change font weight
+                    ),
+                  ),
+                ),
               ),
             ],
           );
@@ -31,14 +62,44 @@ class DialogWidgets {
         context: context,
         builder: (BuildContext context) {
           return AlertDialog(
-            title: Text('Budget Not Exceeded'),
-            content: Text('The budget for $selectedCategory has not been exceeded.'),
+            backgroundColor: Colors.tealAccent,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(30.0), // Add border radius
+              side: BorderSide(
+                color: Colors.black,
+                width: 3.0,
+              ),
+            ),
+            title: const Center(
+              child: Text(
+                'Budget Not Exceeded',
+                style: TextStyle(
+                  color: Colors.black, // Change text color
+                  fontWeight: FontWeight.bold, // Change font weight
+                  fontSize: 20, // Change font size
+                ),
+              ),
+            ),
+            content: Text(
+              'The budget for $selectedCategory has not been exceeded.',
+              style: TextStyle(
+                fontSize: 17,
+              ),
+            ),
             actions: <Widget>[
-              TextButton(
-                onPressed: () {
-                  Navigator.of(context).pop();
-                },
-                child: Text('OK'),
+              Center(
+                child: TextButton(
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                  child: Text(
+                    'OK',
+                    style: TextStyle(
+                      color: Colors.black, // Change text color
+                      fontWeight: FontWeight.bold, // Change font weight
+                    ),
+                  ),
+                ),
               ),
             ],
           );
